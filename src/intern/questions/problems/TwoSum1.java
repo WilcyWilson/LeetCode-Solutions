80f684913547a6c377a7b0bdc5bccc29e38fa98f
wilcywilson <wilsonshresthaapps@gmail.com>
2021-11-24 19:49:35 +0545

    TwoSum1 normal

---
+++ src/intern/questions/problems/TwoSum1.java
@@ -0,0 +1,38 @@
+package intern.questions.problems;
+
+import java.util.Scanner;
+
+public class TwoSum1 {
+
+	public static void main(String[] args) {
+		int n, target;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of elements you want to store: ");
+		n = sc.nextInt();
+		int[] array = new int[n];
+		System.out.println("Enter the elements of the array: ");
+		for (int i = 0; i < n; i++) {
+			array[i] = sc.nextInt();
+		}
+		System.out.print("Enter the target: ");
+		target = sc.nextInt();
+		int[] array2 = new TwoSum1().twoSum(array, target);
+		System.out.println("The output ");
+		System.out.println("[" + array2[0] + "," + array2[1] + "]");
+	}
+
+	public int[] twoSum(int[] nums, int target) {
+		int[] array = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					array[0] = i;
+					array[1] = j;
+					break;
+				}
+			}
+		}
+		return array;
+	}
+
+}
